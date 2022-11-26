90b12e2780550b62a7fd94b5aaa99b9198e6f4c4
m0dB <79429057+m0dB@users.noreply.github.com>
2022-11-26 18:21:07 +0100

    check for db file

--- src/database/mixxxdb.cpp
+++ src/database/mixxxdb.cpp
@@ -1,9 +1,13 @@
 #include "mixxxdb.h"
+#include <iostream>
 
 MixxxDb::MixxxDb()
         : m_db(QSqlDatabase::addDatabase("QSQLITE")) {
     m_db.setDatabaseName("mixxxdb.sqlite");
-    m_db.open();
+    if (!m_db.open()) {
+        std::cerr << "failed to open mixxxdb.sqlite. place a copy in your work directory" << std::endl;
+        exit(-1);
+    }
 }
 
 MixxxDb::~MixxxDb() {
